13ee76be5dc9c5e50fc536d1c516fa15d4c43ae4
Philip Krück <philip.krueck@apploft.de>
2022-04-17 00:51:39 +0200

    added exercise 1-7

---
+++ chapter_1/ex_1-07/write-EOF-value.c
@@ -0,0 +1,14 @@
+/**
+ * Exercise: 1-7
+ * Write a program to print the value of EOF.
+ */
+#include <stdio.h>
+
+int main()
+{
+    printf("The value 'EOF' is: %d\n", EOF);
+}
+
+/**
+ * The value 'EOF' is -1
+ */
