8f17338c493d2f39d00903700bb289d3fdff3fce
Philip Krück <philip.krueck@apploft.de>
2022-04-17 00:46:45 +0200

    added exercise 1-6

---
+++ chapter_1/ex_1-06/write-EOF-value.c
@@ -0,0 +1,14 @@
+/**
+ * Exercise: 1-6
+ * Verify that the expression getchar() != EOF is 0 or 1.
+ */
+#include <stdio.h>
+
+int main()
+{
+    printf("The value of the expression 'getChar() != EOF' is: %d\n", getchar() != EOF);
+}
+
+/**
+ * EOF character is Ctrl+D on Unix and Ctrl+C on Windows.
+ */
