753a84bb267a0204f6ca07e97ba05e7a14e2928b
Philip Krück <philip.krueck@apploft.de>
2022-04-27 15:41:00 +0200

    remove unused code

--- chapter_1/ex_1-17/print_long_lines.c
+++ chapter_1/ex_1-17/print_long_lines.c
@@ -13,9 +13,7 @@ int getLine(char line[], int maxLength);
 int main()
 {
     int currLength = 0;                /* current line length */
-    int maxLength = 0;                 /* maximum line length seen so far */
     char currLine[MAX_CHARS_PER_LINE]; /* current input line */
-    char maxLine[MAX_CHARS_PER_LINE];  /* longest line saved here */
 
     while ((currLength = getLine(currLine, MAX_CHARS_PER_LINE)) > 0)
     {
