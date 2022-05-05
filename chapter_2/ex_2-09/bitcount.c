fc6c4f17a0b775073664f6456e2ef36af9cff4fa
Philip Krück <philip.krueck@apploft.de>
2022-05-05 16:58:59 +0200

    removed superfluous comment

--- chapter_2/ex_2-09/bitcount.c
+++ chapter_2/ex_2-09/bitcount.c
@@ -1,5 +1,5 @@
 /**
- * Exercise 2-8
+ * Exercise 2-9
  * In a two's complement number system, `x &= (x-1)` deletes the rightmost 1-bit in `x`.
  * Explain why. Use this observation to write a faster version of `bitcount`.
  *
@@ -43,9 +43,3 @@ int bitcount(unsigned x)
         b++;
     return b;
 }
-
-/*
- * NOTE: The implemenation has been done for 8-bit integers as this is easy to print out.
- * Conceptually the implementation would work for any size integers with a minor adjustment
- * to line 33.
- */
