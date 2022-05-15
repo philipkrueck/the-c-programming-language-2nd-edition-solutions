6c43e65476b30215f0461573c7ff25bc13b04b50
Philip Krück <philip.krueck@apploft.de>
2022-05-15 10:33:42 +0200

    added solution to ex 4-1

--- chapter_4/ex_4-01/strindex.c
+++ chapter_4/ex_4-01/strindex.c
@@ -23,7 +23,7 @@ int main(void)
 int strindex(char s[], char t[])
 {
     int i, j, k;
-    for (i = strlen(s); i > 0; i--)
+    for (i = strlen(s) - strlen(t); i > 0; i--)
     {
         for (j = i, k = 0; t[k] != '\0' && s[j] == t[k]; j++, k++)
             ;
