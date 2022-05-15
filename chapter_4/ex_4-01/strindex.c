08e10fae8d5e228b9ac5e8006e3b8c80f524b8ad
Philip Krück <philip.krueck@apploft.de>
2022-05-15 10:27:18 +0200

    added solution to ex 4-1

---
+++ chapter_4/ex_4-01/strindex.c
@@ -0,0 +1,34 @@
+/**
+ * Exercise 4-1
+ * Write the function `strindex(s,t)` which returns the position of the rightmost occurrence of `t` in `s`, or -1 if there is none.
+ */
+#include <stdio.h>
+#include <string.h>
+
+/* functions */
+int strindex(char s[], char t[]);
+
+int main(void)
+{
+    char greeting[] = "Hello, how are you? Do you like C?";
+    char name[] = "you";
+
+    int strIndex = strindex(greeting, name);
+
+    printf("The position of the rightmost occurrence of \"%s\" in \"%s\" is pos %d\n", name, greeting, strIndex);
+
+    return strIndex;
+}
+
+int strindex(char s[], char t[])
+{
+    int i, j, k;
+    for (i = strlen(s); i > 0; i--)
+    {
+        for (j = i, k = 0; t[k] != '\0' && s[j] == t[k]; j++, k++)
+            ;
+        if (k > 0 && t[k] == '\0')
+            return i;
+    }
+    return -1;
+}
