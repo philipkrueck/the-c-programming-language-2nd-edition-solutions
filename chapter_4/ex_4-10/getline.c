16b7433ff2b03580d49cb107d16020e9b793f60c
Philip Krueck <philip.krueck@apploft.de>
2022-05-23 09:00:47 +0200

    added exercise description to ex 4-10

--- chapter_4/ex_4-10/getline.c
+++ chapter_4/ex_4-10/getline.c
@@ -1,7 +1,7 @@
 /**
- * Exercise 4-6
- * Add commands for handling variables. (It's easy to provide twenty-six variables
- * with single-letter names.) Add a variable for the most recently printed value.
+ * Exercise 4-10
+ * An alternate organization uses getline to read an entire input line;
+ * this makes getch and ungetch unnecessary. Revise the calculator to use this approach.
  */
 #include <stdio.h>
 #include <ctype.h>
