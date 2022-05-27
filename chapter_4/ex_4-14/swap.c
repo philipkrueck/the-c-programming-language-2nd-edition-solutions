4e2eeefdf23c010daec7a04f49250410b5597020
Philip Krueck <philip.krueck@apploft.de>
2022-05-27 12:52:35 +0200

    added solution for exercise 4-14

---
+++ chapter_4/ex_4-14/swap.c
@@ -0,0 +1,12 @@
+/**
+ * Exercise 4-14
+ * Define  a  macro  swap(t,x,y)
+ * that  interchanges  two  arguments  of  type  t. (Block structure will help.)
+ * */
+#include <stdio.h>
+
+#define swap(t, x, y) ...
+
+int main()
+{
+}
