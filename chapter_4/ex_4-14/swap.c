445375b0fe2702429febcb86dad09a1bc7fb1ac2
Philip Krueck <philip.krueck@apploft.de>
2022-05-27 13:00:06 +0200

    added solution for exercise 4-14

--- chapter_4/ex_4-14/swap.c
+++ chapter_4/ex_4-14/swap.c
@@ -5,8 +5,18 @@
  * */
 #include <stdio.h>
 
-#define swap(t, x, y) ...
+#define swap(t, x, y) \
+    {                 \
+        t tmp = x;    \
+        x = y;        \
+        y = tmp;      \
+    }
 
 int main()
 {
+    int a = 1;
+    int b = 2;
+    printf("before swap: a = %d, b = %d\n", a, b);
+    swap(int, a, b);
+    printf("after swap: a = %d, b = %d\n", a, b);
 }
