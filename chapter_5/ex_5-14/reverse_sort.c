28e705c4b7d75fb631d45a45a5d75edcb633c5dc
Philip Krueck <philip.krueck@apploft.de>
2022-06-06 10:26:14 +0200

    added description to solution of exercise 5-14

--- chapter_5/ex_5-14/reverse_sort.c
+++ chapter_5/ex_5-14/reverse_sort.c
@@ -1,3 +1,8 @@
+/**
+ * Exercise: 5-14
+ * Modify the sort program to handle a -r flag, which indicates sorting in reverse (decreasing) order.
+ * Be sure that -r works with -n.
+ */
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
