6685c106d3bc549ca3418c2b7b1de6c8c5a04cb2
Philip Krueck <philip.krueck@apploft.de>
2022-06-20 13:11:28 +0200

    added solution to ex 6-03

--- chapter_6/ex_6-03/cross_referencer.c
+++ chapter_6/ex_6-03/cross_referencer.c
@@ -1,9 +1,8 @@
 /**
- * Exercise 6-2
- * Write  a  program  that  reads  a  C  program  and  prints  in  alphabetical  order  each  group  of  variable  names
- * that  are  identical  in  the  first  6  characters,  but  different  somewhere  thereafter.
- * Don't  count  words  within  strings  and  comments.
- * Make  6  a  parameter  that  can  be  set from the command line.
+ * Exercise 6-3
+ * Write  a  cross-referencer  that  prints  a  list  of  all  words  in  a  document,
+ * and  for  each  word,  a  list  of  the  line  numbers  on  which  it  occurs.
+ * Remove  noise  words  like  `the  `and`, `the` and so on.
  */
 
 #include <stdio.h>
@@ -11,8 +10,8 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define MAXWORD 100                                /* maximum size of a word */
-#define NKEYS (sizeof keytab / sizeof(struct key)) /* number of keywords */
+#define MAXWORD 100                                     /* maximum size of a word */
+#define NKEYS (sizeof noise_words / sizeof(struct key)) /* number of keywords */
 #define DEFAULT_NUM 6;
 
 struct key
@@ -24,18 +23,25 @@ struct key
 struct tree_node /* the tree node: */
 {
     char *word;              /* points to the text */
-    int count;               /* number of occurrences */
+    struct lines *lines;     /* points to the list of line numbers */
     struct tree_node *left;  /* left child */
     struct tree_node *right; /* right child */
+};
+
+struct lines /* the list of line numbers: */
+{
+    int line_number;
+    struct lines *next;
 };
 
 /* functions */
 int get_word(char *, int);
 struct key *bin_search(char *word, struct key tab[], int n);
-struct tree_node *add_tree_node(struct tree_node *parent, char *word, int num, int *curr_word_matches);
 struct tree_node *tree_node_alloc(void);
-void update_tree_node_if_match(struct tree_node *parent, char *word, int num, int *curr_word_matches);
+struct tree_node *add_tree_node(struct tree_node *parent, char *word, int line_number);
+struct lines *add_lines(struct lines *lines_list, int line_number);
 void print_words_with_lines(struct tree_node *root);
+void print_word_with_lines(char *word, struct lines *lines_list);
 void skip_blanks();
 void skip_comments();
 void skip_char_constant();
@@ -43,55 +49,44 @@ void skip_string_constant();
 
 /* global variables */
 
-struct key keytab[] = {
-    {"auto", 0},
-    {"break", 0},
-    {"case", 0},
-    {"char", 0},
-    {"const", 0},
-    {"continue", 0},
-    {"default", 0},
-    {"do", 0},
-    {"double", 0},
-    {"else", 0},
-    {"enum", 0},
-    {"extern", 0},
-    {"float", 0},
+struct key noise_words[] = {
+    {"and", 0},
+    {"the", 0},
+    {"of", 0},
+    {"in", 0},
+    {"is", 0},
+    {"to", 0},
     {"for", 0},
-    {"goto", 0},
+    {"by", 0},
+    {"on", 0},
+    {"that", 0},
+    {"it", 0},
+    {"this", 0},
+    {"with", 0},
+    {"not", 0},
+    {"or", 0},
+    {"from", 0},
+    {"as", 0},
+    {"was", 0},
+    {"but", 0},
+    {"are", 0},
+    {"be", 0},
     {"if", 0},
-    {"int", 0},
-    {"long", 0},
-    {"register", 0},
-    {"return", 0},
-    {"short", 0},
-    {"signed", 0},
-    {"size_t", 0},
-    {"sizeof", 0},
-    {"static", 0},
-    {"struct", 0},
-    {"switch", 0},
-    {"typedef", 0},
-    {"union", 0},
-    {"unsigned", 0},
-    {"void", 0},
-    {"volatile", 0},
-    {"while", 0},
 };
 
 /* word frequency count */
 int main(int argc, char *argv[])
 {
-    int num = (argc > 1) ? atoi(argv[1]) : DEFAULT_NUM;
-    int curr_word_matches = 0;
     struct tree_node *root = NULL;
     char word[MAXWORD];
+    int line_number = 1;
 
     while (get_word(word, MAXWORD) != EOF)
     {
-        if (isalpha(word[0]) && strlen(word) >= num && !bin_search(word, keytab, NKEYS))
-            root = add_tree_node(root, word, num, &curr_word_matches);
-        curr_word_matches = 0;
+        if (isalpha(word[0]) && !bin_search(word, noise_words, NKEYS))
+            root = add_tree_node(root, word, line_number);
+        else if (word[0] == '\n')
+            line_number++;
     }
 
     print_words_with_lines(root);
@@ -122,13 +117,11 @@ struct key *bin_search(char *word, struct key tab[], int n)
 /* get_word: get next word or character from input */
 int get_word(char *word, int lim)
 {
-    skip_blanks();
-    skip_comments();
-    skip_char_constant();
-    skip_string_constant();
-
-    int c = getchar();
+    char c;
     char *w = word;
+
+    while (isblank(c = getchar()))
+        ;
     if (c != EOF)
         *w++ = c;
     if (!isalpha(c))
@@ -136,8 +129,8 @@ int get_word(char *word, int lim)
         *w = '\0';
         return c;
     }
-    for (; --lim > 0; w++)
-        if (!isalnum(*w = getchar()) && *w != '_')
+    for (; --lim > 0; ++w)
+        if (!isalnum(*w = getchar()))
         {
             ungetc(*w, stdin);
             break;
@@ -149,27 +142,35 @@ int get_word(char *word, int lim)
 /* add_tree_node: update frequency count for word
    if word exists in tree -> increment its count by 1
    if not -> add node in binary tree in lexicographical order with count of 1 */
-struct tree_node *add_tree_node(struct tree_node *parent, char *word, int num, int *curr_word_matches)
+struct tree_node *add_tree_node(struct tree_node *parent, char *word, int line_number)
 {
     if (parent == NULL)
     {
         parent = tree_node_alloc();
         parent->word = strdup(word);
-        parent->count = *curr_word_matches;
+        parent->lines = add_lines(NULL, line_number);
         parent->left = parent->right = NULL;
         return parent;
     }
     else if (strcmp(word, parent->word) < 0)
-    {
-        update_tree_node_if_match(parent, word, num, curr_word_matches);
-        parent->left = add_tree_node(parent->left, word, num, curr_word_matches);
-    }
+        parent->left = add_tree_node(parent->left, word, line_number);
     else if (strcmp(word, parent->word) > 0)
-    {
-        update_tree_node_if_match(parent, word, num, curr_word_matches);
-        parent->right = add_tree_node(parent->right, word, num, curr_word_matches);
-    }
+        parent->right = add_tree_node(parent->right, word, line_number);
+    else
+        parent->lines = add_lines(parent->lines, line_number);
     return parent;
+}
+
+struct lines *add_lines(struct lines *lines_list, int line_number)
+{
+    if (!lines_list)
+    {
+        lines_list = malloc(sizeof(struct lines));
+        lines_list->line_number = line_number;
+    }
+    else if (lines_list->line_number != line_number)
+        lines_list->next = add_lines(lines_list->next, line_number);
+    return lines_list;
 }
 
 /* tree_node_alloc: allocate memory for a tree node */
@@ -178,26 +179,25 @@ struct tree_node *tree_node_alloc(void)
     return (struct tree_node *)malloc(sizeof(struct tree_node));
 }
 
-/* update_tree_node_if_match: update tree node if word matches */
-void update_tree_node_if_match(struct tree_node *parent, char *word, int num, int *curr_word_matches)
-{
-    for (int i = 0; i < num; i++)
-    {
-        if (parent->word[i] != word[i])
-            return;
-    }
-    parent->count = *curr_word_matches = 1;
-}
-
 void print_words_with_lines(struct tree_node *parent)
 {
     if (parent == NULL)
         return;
 
     print_words_with_lines(parent->left);
-    if (parent->count > 0)
-        printf("%s\n", parent->word);
+    print_word_with_lines(parent->word, parent->lines);
     print_words_with_lines(parent->right);
+}
+
+void print_word_with_lines(char *word, struct lines *lines_list)
+{
+    printf("'%s' :", word);
+    while (lines_list != NULL)
+    {
+        printf(" L%d", lines_list->line_number);
+        lines_list = lines_list->next;
+    }
+    printf("\n");
 }
 
 void skip_blanks()
