b2913c185f6f56d627698601e0ea55d9b3197871
Philip Krueck <philip.krueck@apploft.de>
2022-07-04 16:49:03 +0200

    updated exercise description for 6-6

--- src/chapter_6/ex_6-06/define.c
+++ src/chapter_6/ex_6-06/define.c
@@ -1,6 +1,6 @@
 /**
- * Chapter: 6
- * Exercise: 6-06 - Implement a simple version of the #define processor (i.e., no arguments) suitable for use with C programs,
+ * Exercise 6-6
+ * Implement a simple version of the #define processor (i.e., no arguments) suitable for use with C programs,
  * based on the routines of this section. You may also find getch and ungetch helpful.
  **/
 
